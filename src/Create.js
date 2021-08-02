cc928ba70f6b960e4542307025328b2e1d8e7708
Sachu <april.medina2011@gmail.com>
2021-08-02 13:51:22 +0800

    React course completed finished

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,34 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('borge');
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: {  "Content-Type": "application/json"},
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added');
+      setIsPending(false);
+      history.push('/');
+    })
+  }
 
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
           type="text"
@@ -31,8 +51,9 @@ const Create = () => {
           <option value="pot">pot</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
-        <p>{title} {body} {author}</p>
+        { !isPending && <button>Add Blog</button> }
+        { isPending && <button disabled>Adding blog...</button> }
+        {/* <p>{title} {body} {author}</p> */}
       </form>
     </div>
   );
